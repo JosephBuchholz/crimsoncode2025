f7781ad662220b6b181cdd487c28a50610281d05
JosephBuchholz <70069846+JosephBuchholz@users.noreply.github.com>
2025-02-16 00:18:46 -0800

    infinite scrolling

---
+++ movie/src/lib/components/LoadingSpinner.svelte
@@ -0,0 +1,5 @@
+<script>
+	import { Circle } from 'svelte-loading-spinners';
+</script>
+
+<Circle size="60" color="#1e88e5" unit="px" duration="1s" />
--- movie/src/routes/+page.svelte
+++ movie/src/routes/+page.svelte
@@ -1,19 +1,35 @@
 <script lang="ts">
-	import MovieItem from "$lib/components/MovieItem.svelte";
+	import LoadingSpinner from "$lib/components/LoadingSpinner.svelte";
+import MovieItem from "$lib/components/MovieItem.svelte";
 	import Movie from "$lib/Movie";
 	import type { TMDBMovieDetailsItem } from "$lib/server/tmdb";
 	import { onMount } from "svelte";
 
 	let movies: Movie[] = [];
+	let loading = false;
 
-	onMount(async () => {
+	onMount(() => {
+		loadRecommendations();
+		window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+	});
+
+	async function loadRecommendations() {
+		loading = true;
 		const response = await fetch('http://localhost:5173/api/recommend/movies');
 		const data: TMDBMovieDetailsItem[] = (await response.json()).recommendations;
 		data.forEach((recommendation) => {
 			movies.push(Movie.constructFromServerData(recommendation));
 		});
 		movies = movies;
-	});
+		loading = false;
+	}
+
+	function handleScroll() {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 500 && !loading) {
+            loadRecommendations();
+        }
+    }
 </script>
 
 <div class="flex flex-col items-center justify-center w-full h-full">
@@ -24,4 +40,8 @@
             <MovieItem movie={movie}></MovieItem>
         {/each}
     </div>
+
+	{#if loading}
+		<LoadingSpinner></LoadingSpinner>
+    {/if}
 </div>
