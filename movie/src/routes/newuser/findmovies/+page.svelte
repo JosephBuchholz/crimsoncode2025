e27b3f3906d12c58c7eb3df8a99f862c34ab6ea1
Evan Glasscock <121138977+evanglass@users.noreply.github.com>
2025-02-16 02:49:44 -0800

    Search function onboarding movies

--- movie/src/routes/newuser/findmovies/+page.svelte
+++ movie/src/routes/newuser/findmovies/+page.svelte
@@ -3,7 +3,7 @@
 	import Movie from "$lib/Movie";
 	import { onMount } from "svelte";
 	import MovieRatingItem from "./MovieRatingItem.svelte";
-	import type { TMDBMovieDetailsItem } from "$lib/server/tmdb";
+	import type { TMDBMovieSearchItem } from "$lib/server/tmdb";
 
     let movies: Movie[] = [];
 
@@ -11,6 +11,7 @@
 
 	let loading = false;
 	let input = "";
+	let page = 1;
 
 	onMount(() => {
 		loadWatched();
@@ -18,11 +19,22 @@
 
 	async function loadWatched() {
 		loading = true;
-		const response = await fetch(`http://localhost:5173/api/recommend/movies`);
+		movies = [];
+		const response = await fetch(`http://localhost:5173/api/search/movies?q=${input}&page=${page}`);
 
-		const data: TMDBMovieDetailsItem[] = (await response.json()).recommendations;
-		data.forEach((recommendation) => {
-			movies.push(Movie.constructFromServerData(recommendation));
+		const data: TMDBMovieSearchItem[] = (await response.json()).results;
+		data.forEach((el) => {
+			movies.push(
+				new Movie(
+					el.id,
+					el.title,
+					el.overview,
+					`https://image.tmdb.org/t/p/original${el.poster_path}`,
+					Number(el.release_date.split('-')[0]),
+					0,
+					[]
+				)
+			)
 		});
 
 		movies = movies;
@@ -39,6 +51,11 @@
             selectedMovies[movie.id] = movie;
         }
     }
+
+	async function searchChange() {
+		page = 1;
+		await loadWatched();
+	}
 </script>
 
 <div class="flex flex-col" style="height: calc(100vh - 200px)">
@@ -46,7 +63,7 @@
         <h1>Select a few movies that you enjoyed</h1>
     </div>
 
-    <input type="text" placeholder="Search for movies" class="w-full p-2 border border-gray-300 rounded-lg mb-6" bind:value={input}/>
+    <input type="text" placeholder="Search for movies" class="w-full p-2 border border-gray-300 rounded-lg mb-6" on:change={searchChange} bind:value={input}/>
 
     <div class="grid grid-cols-3 gap-4 overflow-y-scroll place-items-center">
         {#each movies as movie}
